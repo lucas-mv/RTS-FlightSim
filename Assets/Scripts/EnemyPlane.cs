ddbcdd36a75caea57b48b45440f19064744149aa
Lucas Moura Veloso <lmouraveloso@gmail.com>
2022-04-23 22:15:32 -0300

    Create enemy plane prefab to spawn a random plane

---
+++ Assets/Scripts/EnemyPlane.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlane : MonoBehaviour
+{
+    [SerializeField] GameObject[] planes;
+
+    private void Start()
+    {
+        planes[Random.Range(0, planes.Length)].SetActive(true);
+    }
+}
