86f92233f1453eb325a71819f91c185a1f5b557c
Lucas Moura Veloso <lmouraveloso@gmail.com>
2022-06-20 23:55:11 -0300

    Improve plane's physics logic

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -284,6 +284,10 @@ public class Plane : MonoBehaviour {
 
         Rigidbody.AddRelativeTorque(new Vector3((liftForceMagnitude + controlInput.x * Mathf.Abs(liftForceMagnitude)) * liftStabilityTorque, 0, 0));
         Rigidbody.AddRelativeTorque(new Vector3(0, (rudderLiftForceMagnitude + controlInput.y * Mathf.Abs(rudderLiftForceMagnitude)) * rudderStabilityTorque, 0));
+
+        //new logic for yaw based on the angle of the airplane
+        Rigidbody.AddRelativeTorque(new Vector3(0, 0, 1) * AngleOfAttackYaw * liftForceMagnitude * 100);
+        Rigidbody.AddForce(new Vector3(1, 0, 0) * _transform.rotation.z * -10 * liftForceMagnitude);
     }
 
     void UpdateAngularDrag() {
